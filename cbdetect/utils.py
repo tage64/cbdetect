9f4b1b2c2b9d87c77bf46cc0dbe39dd377ed1a31
Tage Johansson <tajo9185@student.uu.se>
2024-08-20 09:36:54 +0200

    Add pre-commit hook.

--- cbdetect/utils.py
+++ cbdetect/utils.py
@@ -14,12 +14,15 @@ def piece2id(piece: chess.Piece | None) -> int:
         return 0
     return 6 * int(piece.color) + piece.piece_type
 
+
 def id2piece(id: int) -> chess.Piece | None:
     """Inverse of piece2id()."""
     if id == 0:
         return None
-    return chess.Piece(piece_type=((id - 1) % 6) + 1, color=bool((id-1) // 6))
+    return chess.Piece(piece_type=((id - 1) % 6) + 1, color=bool((id - 1) // 6))
+
+
 # Make sure id2piece() and piece2id() are inverses.
-assert all((piece2id(id2piece(id)) == id for id in range(13)))
+assert all(piece2id(id2piece(id)) == id for id in range(13))
 
 id2label: dict[int, str] = {id: piece2str(id2piece(id)) for id in range(13)}
